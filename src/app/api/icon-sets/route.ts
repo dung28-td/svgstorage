ef2c47c829496f22b79953016bc7a6e189f5d7c5
Dung Tran <dung28.td@gmail.com>
2023-05-01 09:25:54 +0700

    bugfix

--- src/app/api/icon-sets/route.ts
+++ src/app/api/icon-sets/route.ts
@@ -2,7 +2,7 @@ import { readdirSync } from 'fs'
 import { resolve } from 'path'
 import { NextResponse } from 'next/server'
 
-export const iconSets = readdirSync(resolve('public', 'icon-sets')).reduce<Record<string, string[]>>((result, iconSet) => {
+const iconSets = readdirSync(resolve('public', 'icon-sets')).reduce<Record<string, string[]>>((result, iconSet) => {
   const variants = readdirSync(resolve('public', 'icon-sets', iconSet))
   result[iconSet] = variants
   return result
